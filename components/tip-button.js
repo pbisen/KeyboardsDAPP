599e030f23fb97c3b4f8811259f491b68c30903e
pbisen <bisenparth15@gmail.com>
2022-01-07 01:54:24 +0530

    third times the charm eh?

--- components/tip-button.js
+++ components/tip-button.js
@@ -8,16 +8,15 @@ export default function TipButton({ keyboardsContract, index }) {
 
   const submitTip = async (e) => {
     if (!keyboardsContract) {
-      console.error('keyboardsContract object is required to submit a tip');
+      console.error('KeyboardsContract object is required to submit a tip');
       return;
     }
-
+  
     setMining(true);
     try {
-
       const tipTxn = await keyboardsContract.tip(index, { value: ethers.utils.parseEther("0.01") })
       console.log('Tip transaction started...', tipTxn.hash)
-
+  
       await tipTxn.wait();
       console.log('Sent tip!', tipTxn.hash);
     } finally {
